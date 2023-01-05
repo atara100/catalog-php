5d04deffa1cdc0a8794fbcc12c17cded64de7db2
atara100 <al0583230109@gmail.com>
2023-01-05 20:30:23 +0200

    first commit

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php
+include_once "./inc/header.php";
+include_once "./inc/navbar.php";
+?>
+
+<main class="p-4 bg-dark text-white shadow">
+
+    <div class="text-center">
+        <h2>About Sport Superstar</h2>
+        <h5>What a store!</h5>
+    </div>
+
+</main>
+
+<?php
+include_once "./inc/footer.php";
